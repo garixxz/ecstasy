b85bf5f185902a3c0b805d0b60caf99bb368da10
Surajit Saha <surojits005@gmail.com>
2025-04-06 13:00:49 +0530

    deploy

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -157,10 +157,7 @@ function toast({ ...props }: Toast) {
     toast: {
       ...props,
       id,
-      open: true,
-      onOpenChange: (open) => {
-        if (!open) dismiss()
-      },
+     
     },
   })
 
